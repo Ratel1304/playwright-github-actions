630cc3aeea8ac8c24f3490dc148ff3858468e312
Ratel1304 <sana.ratel.fr@gmail.com>
2026-05-23 15:08:40 +0200

    Break test intentionally

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,7 +4,7 @@ test('start-CI', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/faux/);
+  await expect(page).toHaveTitle(/Faux/);
 });
 
 test('get started link', async ({ page }) => {
